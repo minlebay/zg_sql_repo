a39749550b00f8a5bec2d91bb67297f94650b3e7
Ilshat Minnibaev <ilshatminnibaev@gmail.com>
2024-08-03 22:45:12 +0200

    default config vals from env

--- internal/app/config.go
+++ internal/app/config.go
@@ -1,11 +1,9 @@
 package app
 
 import (
-	"fmt"
 	"go.uber.org/config"
 	"go.uber.org/fx"
 	"os"
-	"strings"
 )
 
 type Config struct {
@@ -19,97 +17,24 @@ type ResultConfig struct {
 }
 
 func NewConfig() (ResultConfig, error) {
-	yamlProvider, err := config.NewYAML(config.File("config.yaml"))
+	yamlProvider, err := config.NewYAML(
+		config.File("config.yaml"),
+		config.Expand(os.LookupEnv),
+	)
 	if err != nil {
 		return ResultConfig{}, err
 	}
 
-	var yamlConfig map[interface{}]interface{}
-	if err = yamlProvider.Get(config.Root).Populate(&yamlConfig); err != nil {
-		return ResultConfig{}, err
-	}
-
-	stringMap := convertMapKeysToStrings(yamlConfig)
-	replaceEnvVariables(stringMap)
-
-	provider, err := config.NewYAML(config.Static(stringMap))
-	if err != nil {
-		return ResultConfig{}, err
-	}
-
-	config := Config{
+	cfg := Config{
 		Name: "default",
 	}
 
-	if err = provider.Get("app").Populate(&config); err != nil {
+	if err = yamlProvider.Get("app").Populate(&cfg); err != nil {
 		return ResultConfig{}, err
 	}
 
 	return ResultConfig{
-		Provider: provider,
-		Config:   config,
+		Provider: yamlProvider,
+		Config:   cfg,
 	}, nil
-}
-
-func convertMapKeysToStrings(m map[interface{}]interface{}) map[string]interface{} {
-	newMap := make(map[string]interface{})
-	for k, v := range m {
-		strKey := fmt.Sprintf("%v", k)
-		switch val := v.(type) {
-		case map[interface{}]interface{}:
-			newMap[strKey] = convertMapKeysToStrings(val)
-		case []interface{}:
-			newMap[strKey] = convertSliceKeysToStrings(val)
-		default:
-			newMap[strKey] = val
-		}
-	}
-	return newMap
-}
-
-func convertSliceKeysToStrings(val []interface{}) interface{} {
-	newSlice := make([]interface{}, len(val))
-	for i, v := range val {
-		switch v := v.(type) {
-		case map[interface{}]interface{}:
-			newSlice[i] = convertMapKeysToStrings(v)
-		case []interface{}:
-			newSlice[i] = convertSliceKeysToStrings(v)
-		default:
-			newSlice[i] = v
-		}
-	}
-	return newSlice
-}
-
-func replaceEnvVariables(config map[string]interface{}) {
-	for key, value := range config {
-		switch v := value.(type) {
-		case map[string]interface{}:
-			replaceEnvVariables(v)
-		case []interface{}:
-			replaceEnvVariablesInSlice(v)
-		case string:
-			if strings.HasPrefix(v, "${") && strings.HasSuffix(v, "}") {
-				envVar := v[2 : len(v)-1]
-				config[key] = os.Getenv(envVar)
-			}
-		}
-	}
-}
-
-func replaceEnvVariablesInSlice(s []interface{}) {
-	for i, value := range s {
-		switch v := value.(type) {
-		case map[string]interface{}:
-			replaceEnvVariables(v)
-		case []interface{}:
-			replaceEnvVariablesInSlice(v)
-		case string:
-			if strings.HasPrefix(v, "${") && strings.HasSuffix(v, "}") {
-				envVar := v[2 : len(v)-1]
-				s[i] = os.Getenv(envVar)
-			}
-		}
-	}
 }
